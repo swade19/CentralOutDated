4265d49159f1b5faecb94838ac2145425e14b5a1
swade19 <sarah.es@hotmail.co.uk>
2022-10-09 12:36:35 +0100

    Front end Changes up to date

---
+++ Central/Central.Data/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Central.Data.Models
+{
+    public class Location
+    {
+        public string Place { get; set; }
+    }
+}
---
+++ Central/Central.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Central.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Central.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        InMemoryTodoData db;
+
+        public HomeController()
+        {
+            db = new InMemoryTodoData(); 
+        }
+
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
